e1cc8b3f39c8e22e84ff4f8e061a8691846bff83
Peter Kanev <pip3r4o@gmail.com>
2018-01-25 11:39:07 +0200

    refactor(project-data): read app_resources dir from nsconfig.json
    Refactor the project-data service to read a config.json for the
    'app_resources' property entry, which points relatively to the
    App_Resources directory.
    
    All references to app/App_Resources now retrieve the path from
    said service.
    
    To keep the service backwards compatible, if no config.json is
    present, or the app_resources entry isn't available, location
    defaults to projectDir/app/App_Resources.

--- lib/definitions/project.d.ts
+++ lib/definitions/project.d.ts
@@ -61,7 +61,6 @@ interface IProjectData extends IProjectDir {
 	dependencies: any;
 	devDependencies: IStringDictionary;
 	appDirectoryPath: string;
-	appResourcesDirectoryPath: string;
 	projectType: string;
 	/**
 	 * Initializes project data with the given project directory. If none supplied defaults to --path option or cwd.
